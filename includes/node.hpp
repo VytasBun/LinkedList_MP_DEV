ee03572c377e7d47b728c3687d517956224e3aa2
Sam Li <sli262@illinois.edu>
2025-04-17 21:40:32 +0000

    Added node struct

---
+++ includes/node.hpp
@@ -0,0 +1,15 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+#include "pixel.hpp"
+
+struct Node {
+  Pixel pixel_;
+  unsigned int count_ = 0;
+  Node* next_ = nullptr;  // NOLINT
+  Node(Pixel pixel): pixel_(pixel) {}
+  Node(Pixel pixel, unsigned int count): pixel_(pixel), count_(count) {}
+  Node(Pixel pixel, unsigned int count, Node* next):
+      pixel_(pixel), count_(count), next_(next) {}
+};
+
+#endif

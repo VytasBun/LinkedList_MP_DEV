8a7772cb450ff693ec96bb9fd3be98a18b02650d
Sam Li <sli262@illinois.edu>
2025-04-17 21:46:13 +0000

    More constructors for node struct

--- includes/node.hpp
+++ includes/node.hpp
@@ -10,6 +10,11 @@ struct Node {
   Node(Pixel pixel, unsigned int count): pixel_(pixel), count_(count) {}
   Node(Pixel pixel, unsigned int count, Node* next):
       pixel_(pixel), count_(count), next_(next) {}
+  Node(int r, int g, int b): pixel_(Pixel(r, g, b)) {}
+  Node(int r, int g, int b, unsigned int count):
+      pixel_(Pixel(r, g, b)), count_(count) {}
+  Node(int r, int g, int b, unsigned int count, Node* next):
+      pixel_(Pixel(r, g, b)), count_(count), next_(next) {}
 };
 
 #endif

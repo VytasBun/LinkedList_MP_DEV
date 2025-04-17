db8be9096be8b9a39905142a0ba701b3ddf7a6c9
VytasBun <vytasbuntinas@gmail.com>
2025-04-17 15:49:16 -0500

    Test changes

--- src/driver.cc
+++ src/driver.cc
@@ -4,6 +4,7 @@
 int main() {
   // constexpr char kConsonants[] = {'p', 'k', 'h', 'l', 'm', 'n', 'w'};
   std::map<int, char> example = {{1, 'a'}, {2, 'b'}};
+  std::cout << "Hello" << std::endl;
 
   for (int x : {2, 5}) {
     if (example.contains(x)) {

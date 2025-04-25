922c7c5fd32e7053c3337285a37157d76dc9e490
Sam Li <sli262@illinois.edu>
2025-04-25 07:06:20 +0000

    actually fixed csv input (1)

--- src/driver.cc
+++ src/driver.cc
@@ -3,6 +3,15 @@
 #include <map>
 #include <rle_image_ppm.hpp>
 
+int char_count(const std::string& file_name) {
+  std::ifstream ifs{file_name};
+  int ans = 0;
+  char c;
+  while (ifs.get(c)) {
+    ans++;
+  }
+  return ans;
+}
 int main() {
   RLEImagePPM rle("images/CS128Logo400x308.ppm");
   // rle.CropRight(50);
@@ -10,6 +19,10 @@ int main() {
   rle.FlipVertical();
   // rle.ToPPM("output.ppm");
   rle.ToCSV("out.csv");
+  rle.ToPPM("output_right.ppm");
   RLEImagePPM rle2("out.csv", 400, 308, 65535);
   rle2.ToPPM("output.ppm");
+
+  std::cout << "Char Count for PPM: " << char_count("output.ppm") << std::endl;
+  std::cout << "Char Count for CSV: " << char_count("out.csv") << std::endl;
 }
